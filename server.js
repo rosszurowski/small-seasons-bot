114be19eabec67a837479d7786d4ef212efa5808
Ross Zurowski <ross@rosszurowski.com>
2017-07-23 13:28:27 -0700

    Fix lots of things

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 
 require('dotenv').config();
 
-const micro = require('micro');
 const schedule = require('node-schedule');
 const twitter = require('./lib/twitter');
 const sekkis = require('./sekki.json');
 
-const parseStartDate = str => {
-  const [month, day] = str.split('-');
+const parseStartDate = (str) => {
+  const [month, day] = str.split('-').map(n => parseInt(n, 10));
   return { month, day };
-}
+};
 
-const toCronFormat = ({ month, day }) => `5 20 ${day} ${month} *`;
+const toCronFormat = ({ month, day }) => `02 16 ${day} ${month} *`;
 
 const instructions = sekkis.map(sekki => ({
   id: sekki.id,
@@ -19,18 +18,21 @@ const instructions = sekkis.map(sekki => ({
   tweet: `${sekki.title}. ${sekki.description} ${sekki.emoji}`,
 }));
 
-const getPostTweetJob = tweet => () => twitter.post(tweet);
+const getPostTweetJob = tweet => () =>
+  twitter.post(tweet)
+    .then(() => console.log('Posted tweet'))
+    .catch(err => console.error(err));
 
-const jobs = instructions.map(instruction => schedule.scheduleJob(
+instructions.map(instruction => schedule.scheduleJob(
   instruction.crontab,
-  getPostTweetJob(instructions.tweet)
+  getPostTweetJob(instruction.tweet),
 ));
 
 // Keep alive...
 module.exports = () => `
   <html lang="en">
     <body>
-      <p>Go follow <a href="https://twitter.com/smallseasonsbot">@smallseasonsbot</a>!  </p>
+      <p>Go follow <a href="https://twitter.com/smallseasonsbot">@smallseasonsbot</a>!</p>
     </body>
   </html>
 `;
