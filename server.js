d5f91ebe1ceb7e4ac5610ef9df1aa93e347cc2aa
Ross Zurowski <ross@rosszurowski.com>
2018-12-17 09:07:07 +0900

    Move to Heroku

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
 require('dotenv').config();
 
 const schedule = require('node-schedule');
 const twitter = require('./lib/twitter');
 const sekkis = require('./sekki.json');
 
-const parseStartDate = (str) => {
+const parseStartDate = str => {
   const [month, day] = str.split('-').map(n => parseInt(n, 10));
   return { month, day };
 };
@@ -15,18 +14,18 @@ const toCronFormat = ({ month, day }) => `02 16 ${day} ${month} *`;
 const instructions = sekkis.map(sekki => ({
   id: sekki.id,
   crontab: toCronFormat(parseStartDate(sekki.startDate)),
-  tweet: `${sekki.title}. ${sekki.description} ${sekki.emoji}`,
+  tweet: `${sekki.title}. ${sekki.description} ${sekki.emoji}`
 }));
 
 const getPostTweetJob = tweet => () =>
-  twitter.post(tweet)
+  twitter
+    .post(tweet)
     .then(() => console.log('Posted tweet'))
-    .catch(err => console.error(err));
+    .catch(error => console.error(error));
 
-instructions.map(instruction => schedule.scheduleJob(
-  instruction.crontab,
-  getPostTweetJob(instruction.tweet),
-));
+instructions.map(instruction =>
+  schedule.scheduleJob(instruction.crontab, getPostTweetJob(instruction.tweet))
+);
 
 // Keep alive...
 module.exports = () => `
